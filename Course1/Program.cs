159b199ef9c44432af8e283da4b844a5a136465b
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2024-02-16 00:13:17 +0100

    First commit. Exercise files from c# course taken, will continue later.

---
+++ Course1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Course1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] cars = { "Toyota", "BMW", "Nissan" };
+
+
+        for (int i = 0; i < cars.Length; i++)
+        {
+            Console.WriteLine(cars[i]);
+        }
+        
+        
+        Console.ReadKey();
+    }
+ }
---
+++ Course1/WrittenCode.cs
@@ -0,0 +1,55 @@
+﻿namespace Course1;
+
+public class WrittenCode
+{
+    /* bool True = true;
+        while (True)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            bool containDigit = name.Any(Char.IsDigit);
+            if (!containDigit)
+            {
+                Console.WriteLine($"Nice to meet you {name}");
+                True = false;
+            }
+            else
+            {
+                Console.WriteLine("Your name does not contain any digits you mfer");
+            }
+        }
+
+        Console.WriteLine("Welcome out of the loop, stranger. ");
+
+        */
+        
+    /* string day = Console.ReadLine();
+
+    switch (day)
+    {
+        case "Monday":
+            Console.WriteLine("niger smokey");
+            break;
+        case "Bread":
+            Console.WriteLine("fuck you");
+            break;
+        default:
+            Console.WriteLine(":3");
+            break;
+    } */
+    
+    /*
+     * bool bread = true;
+        while (bread)
+        {
+            for (int i = 0; i < 15    ; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            bread = false;
+        }
+
+        Console.WriteLine("loops are for dumbis");
+     */
+}
